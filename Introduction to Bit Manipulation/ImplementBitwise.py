56c070a0ead741fa63ec03ff30e6653d9008a444
Veer Singh <v1014singh@gmail.com>
2025-06-13 20:02:41 +0530

    inn

---
+++ Introduction to Bit Manipulation/ImplementBitwise.py
@@ -0,0 +1,19 @@
+num1 = 100
+num2 = 10
+
+print("num1 & num2:", num1 & num2)
+print("\nnum1 | num2:", num1 | num2)
+print("\n~num1 ^ num2:", num1 ^ num2)
+print("\nnum1 ^ num2:", num1 ^ num2)
+
+num1 = 100
+num2 = 10
+
+print("\nnum >> 2:", num1 >> 2)
+print("\nnum2 >> 1:", num2 >> 1)
+
+num1 = 100
+num2 = 10
+
+print("\nnum1 << 2:", num1 << 2)
+print("\nnum2 << 1:", num2 << 1)
