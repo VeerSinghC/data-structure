712709f5c2771a2ea7b7013d900a59359509846a
Veer Singh <v1014singh@gmail.com>
2025-06-13 20:07:22 +0530

    inn

---
+++ Introduction to Bit Manipulation/Odd Even BItwise.py
@@ -0,0 +1,12 @@
+def isEvenOdd(n):
+    if (n ^ 1 == n+1):
+        return True;
+    else:
+        return False;
+
+number = int(input("Enter a number: "))
+
+if isEvenOdd(number):
+ print(number, "is an even number")
+else:
+ print(number, "is an odd number")        
